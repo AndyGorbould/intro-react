b2d6e229601d6f97493a5b770d2a08ad5f6c5f15
Andy Gorbould <gorbould@mail.com>
2022-09-13 11:00:38 +0200

    basic input form ready

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "./InputForm.module.css";
 
 const InputForm = () => (
-  <div className={styles.InputForm}>InputForm Component</div>
+  <div className={styles.InputForm}>
+    <form>
+      <label htmlFor="">
+        Add To-Do:
+        <input type="text" name="addToDo" />
+      </label>
+      <input type="submit" value="submit" />
+    </form>
+  </div>
 );
 
 export default InputForm;
